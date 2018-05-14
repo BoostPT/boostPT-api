71fc76d805830700eec8250bdd980f8eafe10499
DChan0319 <chaosstriker0319@gmail.com>
2018-05-14 12:00:13 -0700

    Fixed bugs after rebasing.

--- rest-server/src/components/users/userControllers.js
+++ rest-server/src/components/users/userControllers.js
@@ -2,8 +2,9 @@ import { globalController } from '../../lib/utils/queryHelpers';
 import { fetchClientNonUserQuery } from './userQueries';
 import {addClientNonUserQuery} from './userQueries';
 import { userQuery } from './userQueries';
+import {userAddPictureQuery} from './userQueries';
 
 export const fetchNonUserClientsController = globalController(fetchClientNonUserQuery, 'nonUserClientsController');
 export const addNonUserClientsController = globalController(addClientNonUserQuery, 'addClientNonUserController');
-export const userPictureEdit = globalController(userQuery, 'userPictureEdit');
+export const userPictureEdit = globalController(userAddPictureQuery, 'userPictureEdit');
 export const userController = globalController(userQuery , 'userController');
