6c176ce1e5e3265554c658e0f2c8e1b685775d95
Aaron Melendez <aaronmelendez37@gmail.com>
2018-05-14 11:11:55 -0700

    Committing for rebase

--- rest-server/src/components/workouts/workoutQueries.js
+++ rest-server/src/components/workouts/workoutQueries.js
@@ -15,5 +15,7 @@ export const workoutQuery = async (payload, url) => {
     return await globalQueryHelper(payload, fetchExercisesByWorkout, 'fetchExercisesByWorkout', ['workout_id']);
   } else if (url.slice(0,5) === '/user') {
     return await globalQueryHelper(payload, fetchWorkoutsByUser, 'fetchWorkoutsByUser', ['user_id']);
+  } else if (url.slice(0, 9) === '/workouts') {
+    console.log('made it to workout queries')
   }
 };
