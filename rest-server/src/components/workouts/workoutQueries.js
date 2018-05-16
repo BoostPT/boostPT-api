4549a7cb2ae35e9c600523e5641a1a59f42a6ca2
Aaron Melendez <aaronmelendez37@gmail.com>
2018-05-16 10:04:41 -0700

    wrapped workout queries in try catch statements

--- rest-server/src/components/workouts/workoutQueries.js
+++ rest-server/src/components/workouts/workoutQueries.js
@@ -17,21 +17,33 @@ import {
 
 export const workoutQuery = async (payload, url) => {
   if (url.slice(0,10) === '/exercises') {
-    return await globalQueryHelper(payload, fetchExercisesByWorkout, 'fetchExercisesByWorkout', ['workout_id']);
-  } else if (url.slice(0,5) === '/user') {
-    return await globalQueryHelper(payload, fetchWorkoutsByUser, 'fetchWorkoutsByUser', ['user_id']);
-  } else if (url.slice(0, 7) === '/delete') {
-    // query for exercises by workout id
-    const exercises = await globalQueryHelper(payload, fetchExerciseIdsByWorkout, 'fetchExerciseIdsByWorkout', ['workout_id']);
-    await globalQueryHelper(payload, deleteFromExerciseWorkout, 'deleteFromExerciseWorkout', ['workout_id']);
-    await globalQueryHelper(payload, deleteFromUsersWorkouts, 'deleteFromUsersWorkouts', ['workout_id']);
-    // iterate over exercises and delete each from exercises
-    let i = 0;
-    while (i < exercises.rows.length) {
-      payload.id = exercises.rows[i].id;
-      await globalQueryHelper(payload, deleteFromExercises, 'deleteFromExercises', ['id']);
-      i++;
+    try {
+      return await globalQueryHelper(payload, fetchExercisesByWorkout, 'fetchExercisesByWorkout', ['workout_id']);
+    } catch (err) {
+      return err;
     }
-    await globalQueryHelper(payload, deleteFromWorkouts, 'deleteFromWorkouts', ['workout_id']);
+  } else if (url.slice(0,5) === '/user') {
+    try {
+      return await globalQueryHelper(payload, fetchWorkoutsByUser, 'fetchWorkoutsByUser', ['user_id']);
+    } catch (err) {
+      return err;
+    }
+  } else if (url.slice(0, 7) === '/delete') {
+    try {
+      // query for exercises by workout id
+      const exercises = await globalQueryHelper(payload, fetchExerciseIdsByWorkout, 'fetchExerciseIdsByWorkout', ['workout_id']);
+      await globalQueryHelper(payload, deleteFromExerciseWorkout, 'deleteFromExerciseWorkout', ['workout_id']);
+      await globalQueryHelper(payload, deleteFromUsersWorkouts, 'deleteFromUsersWorkouts', ['workout_id']);
+      // iterate over exercises and delete each from exercises
+      let i = 0;
+      while (i < exercises.rows.length) {
+        payload.id = exercises.rows[i].id;
+        await globalQueryHelper(payload, deleteFromExercises, 'deleteFromExercises', ['id']);
+        i++;
+      }
+      await globalQueryHelper(payload, deleteFromWorkouts, 'deleteFromWorkouts', ['workout_id']);
+    } catch (err) {
+      return err;
+    }
   }
 };
