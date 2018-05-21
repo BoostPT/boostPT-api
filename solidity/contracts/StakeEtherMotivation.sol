7ae6efb72c547ecba3ceb681ebee1a0de5beeffc
Zach Gallagher <zachagallagher@gmail.com>
2018-05-21 15:08:40 -0700

    Successfully creating incentives and fetching them, with 2 passed tests

--- solidity/contracts/StakeEtherMotivation.sol
+++ solidity/contracts/StakeEtherMotivation.sol
@@ -1,9 +1,11 @@
 pragma solidity ^0.4.23;
-pragma experimental ABIEncoderV2;
 
 import "./openzeppelin/Destructible.sol";
+import "./openzeppelin/SafeMath.sol";
 
 contract StakeEtherMotivation is Destructible {
+
+    using SafeMath for uint256;
 
     event IncentiveCreated(
         address indexed _from,
@@ -37,13 +39,18 @@ contract StakeEtherMotivation is Destructible {
         uint _staked;
     }
 
+    // Map each user to list of incentives
     mapping (address => Incentive[]) incentives;
+
+    // Map each user to number of incentives
+    mapping (address => uint) incentiveCount;
 
     function createIncentive(address recipient, uint deadline) public payable returns (bytes32) {
         require(deadline > now);
         bytes32 incentiveId = keccak256(msg.sender, recipient, deadline, now);
         Incentive memory newIncentive = Incentive(incentiveId, msg.sender, recipient, deadline, msg.value);
         incentives[msg.sender].push(newIncentive);
+        incentiveCount[msg.sender] = incentiveCount[msg.sender].add(1);
         emit IncentiveCreated(msg.sender, recipient, deadline, msg.value);
         return incentiveId;
     }
@@ -54,37 +61,44 @@ contract StakeEtherMotivation is Destructible {
             userIncentives[i] = userIncentives[i + 1];
         }
         delete userIncentives[userIncentives.length-1];
+        incentiveCount[msg.sender] = incentiveCount[msg.sender].sub(1);
         userIncentives.length--;
     }
 
-    function _getCount(Incentive[] incentiveArray) pure internal returns (uint) {
-        return incentiveArray.length;
-    }
+    function fetchIncentives() view public returns (bytes32[], address[], address[], uint[], uint[]) {
 
-    function fetchIncentives() public returns (Incentive[]) {
-        // Loop through existing incentives, if deadline is < now, transfer payment to recipient, emit event,
-        // and delete expired incentives from the end of the array.
-        Incentive[] memory userIncentives = incentives[msg.sender];
-        for (uint i = _getCount(userIncentives) - 1; i >= 0 ; i--) {
-            require(incentives[msg.sender][i]._creator == msg.sender);
-            if (userIncentives[i]._deadline < now) {
-                emit IncentiveExpiredAndPayed(msg.sender,
-                    userIncentives[i]._recipient,
-                    userIncentives[i]._deadline,
-                    userIncentives[i]._staked);
-                userIncentives[i]._recipient.transfer(userIncentives[i]._staked);
-                _removeIncentiveHelper(incentives[msg.sender], i);
-            }
+        bytes32[] memory incentiveIds = new bytes32[](incentiveCount[msg.sender]);
+        address[] memory creators = new address[](incentiveCount[msg.sender]);
+        address[] memory recipients = new address[](incentiveCount[msg.sender]);
+        uint[] memory deadlines = new uint[](incentiveCount[msg.sender]);
+        uint[] memory stakes = new uint[](incentiveCount[msg.sender]);
+
+        for (uint i = 0; i < incentiveCount[msg.sender]; i++) {
+            incentiveIds[i] = incentives[msg.sender][i]._incentiveId;
+            creators[i] = incentives[msg.sender][i]._creator;
+            recipients[i] = incentives[msg.sender][i]._recipient;
+            deadlines[i] = incentives[msg.sender][i]._deadline;
+            stakes[i] = incentives[msg.sender][i]._staked;
         }
 
-        return incentives[msg.sender];
+        return (incentiveIds, creators, recipients, deadlines, stakes);
     }
-
-    // Duplicating logic for fulfilling/cancelling incentives to avoid error passing events as a function param
 
     function fulfillIncentive(bytes32 incentiveId) public {
         for (uint i = 0; i < incentives[msg.sender].length; i++) {
             require(incentives[msg.sender][i]._creator == msg.sender);
+
+            // If incentive is expired, transfer payment to recipient, emit event, and delete incentive
+            if (incentives[msg.sender][i]._deadline < now) {
+                emit IncentiveExpiredAndPayed(msg.sender,
+                    incentives[msg.sender][i]._recipient,
+                    incentives[msg.sender][i]._deadline,
+                    incentives[msg.sender][i]._staked);
+                incentives[msg.sender][i]._recipient.transfer(incentives[msg.sender][i]._staked);
+                _removeIncentiveHelper(incentives[msg.sender], i);
+            }
+
+            // Refund staked ether
             if (incentives[msg.sender][i]._incentiveId == incentiveId && incentives[msg.sender][i]._deadline > now) {
                 emit IncentiveFulfilledandRefunded(msg.sender,
                 incentives[msg.sender][i]._recipient,
@@ -100,6 +114,18 @@ contract StakeEtherMotivation is Destructible {
     function cancelIncentive(bytes32 incentiveId) public {
         for (uint i = 0; i < incentives[msg.sender].length; i++) {
             require(incentives[msg.sender][i]._creator == msg.sender);
+
+            // If incentive is expired, transfer payment to recipient, emit event, and delete incentive
+            if (incentives[msg.sender][i]._deadline < now) {
+                emit IncentiveExpiredAndPayed(msg.sender,
+                    incentives[msg.sender][i]._recipient,
+                    incentives[msg.sender][i]._deadline,
+                    incentives[msg.sender][i]._staked);
+                incentives[msg.sender][i]._recipient.transfer(incentives[msg.sender][i]._staked);
+                _removeIncentiveHelper(incentives[msg.sender], i);
+            }
+
+            // Refund staked ether
             if (incentives[msg.sender][i]._incentiveId == incentiveId && incentives[msg.sender][i]._deadline > now) {
                 emit IncentiveCancelledandRefunded(msg.sender,
                     incentives[msg.sender][i]._recipient,
@@ -111,32 +137,5 @@ contract StakeEtherMotivation is Destructible {
             }
         }
     }
-
-
-    // Original implementation consolidating fulfilling/cancelling logic into one helper
-
-
-//    function _deleteUserIncentive(bytes32 _incentiveId, event _IncentiveFulfilledOrCancelled) internal {
-//        for (uint i = 0; i < incentives[msg.sender].length; i++) {
-//            require(incentives[msg.sender][i]._creator == msg.sender);
-//            if (incentives[msg.sender][i]._incentiveId == _incentiveId && incentives[msg.sender][i]._deadline > now) {
-//                emit _IncentiveFulfilledOrCancelled(msg.sender,
-//                incentives[msg.sender][i]._recipient,
-//                incentives[msg.sender][i]._deadline,
-//                incentives[msg.sender][i]._staked);
-//                msg.sender.transfer(incentives[msg.sender][i]._staked);
-//                _removeIncentiveHelper(incentives[msg.sender], i);
-//                break;
-//            }
-//        }
-//    }
-//
-//    function fulfillIncentive(bytes32 incentiveId) public {
-//        _deleteUserIncentive(incentiveId, IncentiveFulfilledandRefunded);
-//    }
-//
-//    function cancelIncentive(bytes32 incentiveId) public {
-//        _deleteUserIncentive(incentiveId, IncentiveCancelledandRefunded);
-//    }
 
 }
--- solidity/test/stakeEtherMotivation-test.js
+++ solidity/test/stakeEtherMotivation-test.js
@@ -2,23 +2,39 @@ const StakeEtherMotivation = artifacts.require("StakeEtherMotivation");
 
 contract('StakeEtherMotivation test', async (accounts) => {
 
-  it('should have a balance of 0', async () => {
-    let instance = await StakeEtherMotivation.deployed();
-    let balance = await instance.getBalance.call(accounts[0]);
-    assert.equal(balance.valueOf(), 0);
+  let instance;
+
+  beforeEach('instantiate contract for each test', async () => {
+    instance = await StakeEtherMotivation.deployed();
   });
 
-  it('should return the staked ether after a cancellation', async () => {
-    let instance = await StakeEtherMotivation.deployed();
-
-
-
+  it('initializes with the right owner', async () => {
+    const owner = await instance.owner();
+    assert.equal(owner, 0xCbdC7A852494eb6B4BcB44F114D2396AcAe15668, 'owner should be the deployer');
   });
 
-  it('should send the staked ether after the deadline', async () => {
-    let instance = await StakeEtherMotivation.deployed();
+  it('creates an incentive and fetches it successfully', async () => {
+    await instance.createIncentive(
+      0x7B0b0185E088B8f76171b78fA2Ee7C383F4Af940,
+      Date.now()/1000 + 60 * 60 * 24,
+      { from: accounts[0], value: web3.toWei(0.08, 'ether')});
 
+    const result = await instance.fetchIncentives();
 
+    console.log(result)
   });
+
+  // it('should return the staked ether after a cancellation', async () => {
+  //
+  //
+  //
+  //
+  // });
+  //
+  // it('should send the staked ether after the deadline', async () => {
+  //
+  //
+  //
+  // });
 
 });
