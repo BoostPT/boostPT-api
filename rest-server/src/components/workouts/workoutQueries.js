799b2a60e6c508dc652617f87e107d43eaa0f13b
Aaron Melendez <aaronmelendez37@gmail.com>
2018-05-24 14:59:14 -0700

    Imported relevant queries

--- rest-server/src/components/workouts/workoutQueries.js
+++ rest-server/src/components/workouts/workoutQueries.js
@@ -12,7 +12,9 @@ import {
   getStarredExercisesByUser,
   fetchPublicWorkoutsByUser,
   starWorkoutExistance,
-  deleteStarWorkout
+  deleteStarWorkout,
+  starExerciseExistance,
+  deleteStarExercise
 } from './workoutSQLHelpers';
 
 export const workoutQuery = async (payload, url) => {
@@ -39,6 +41,7 @@ export const workoutQuery = async (payload, url) => {
       let i = 0;
       while (i < exercises.rows.length) {
         payload.exercise_id = exercises.rows[i].id;
+        console.log('MADE IT HERE!', payload)
         const star = await globalQueryHelper(payload, starExerciseExistance, 'starExerciseExistance', ['exercise_id', 'user_id']);
         if (star.rows[0].exists) {
           await globalQueryHelper(payload, deleteStarExercise, 'deleteStarExercise', ['exercise_id', 'user_id']);
